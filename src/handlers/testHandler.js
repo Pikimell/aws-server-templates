cb090fc09992e2e260397671711be66bc8aeb2ae
Volodymyr Pashchenko <volodkaposhta@gmail.com>
2025-02-02 12:33:17 +0200

    update

--- src/handlers/testHandler.js
+++ src/handlers/testHandler.js
@@ -1,4 +1,17 @@
-export const updateGroupHandler = async (event, context) => {
-  const ctrl = ctrlWrapper(authController.updateGroup);
+import { testController } from '../controllers/testController';
+import { ctrlWrapper } from '../utils/ctrlWrapper';
+
+export const testHandler = async (event, context) => {
+  const ctrl = ctrlWrapper(testController);
+  return await ctrl(event, context);
+};
+
+export const testHandler2 = async (event, context) => {
+  const ctrl = ctrlWrapper(testController);
+  return await ctrl(event, context);
+};
+
+export const testHandler3 = async (event, context) => {
+  const ctrl = ctrlWrapper(testController);
   return await ctrl(event, context);
 };
